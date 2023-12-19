e0688d39c8275481eb4984345fa27d697845647d
Albin Biju <albin.biju@experionglobal.local>
2023-12-19 22:47:50 +0530

    fix: changed some style

--- script/language.js
+++ script/language.js
@@ -5,6 +5,11 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNjdhZDgwMjUxNDdlZThkY2ZiYjZjY2ZiNTIxNDMzNCIsInN1YiI6IjYzN2E0ZDgwOTc2ZTQ4MDBiNDU1ZDI1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iz4uW_emM4tQei3loe66oPYe0Te_HpcSKpfj8u921fk'
     }
   };
+
+window.onload = () =>
+{
+  selectLanguage();
+}
   
   
   const selectLanguage = () => {
