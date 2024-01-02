9134a5309b90e3e027b3651ab55089ce95d3d556
Albin Biju <albin.biju@experionglobal.local>
2024-01-02 14:15:29 +0530

    feat: added profiles,authentication and some styles

---
+++ manageprofile/manageprofile.js
@@ -0,0 +1,31 @@
+import { firebaseConfig } from "../script/config.js"
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+
+import { getFirestore, collection, getDocs, doc , arrayUnion, updateDoc} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+
+const app = initializeApp(firebaseConfig);
+  
+const db = getFirestore(app);
+
+window.onload = async () => {
+    const colRef = collection(db, "profileCollection");
+    const docRef = doc(colRef,"user1");
+    const querySnapshot = await getDocs(collection(docRef, "profiles"));
+    const parentElement = document.getElementById('profiledetailsid');
+    let count = 1;
+  querySnapshot.forEach((doc) => {
+    const childElement = document.createElement('div');
+    childElement.className = "profileinnerdetails d-flex flex-column me-3";
+    childElement.innerHTML = `<div class="profileimage bg-primary" id="profile${count++}"></div><p class="iconimagep">${doc.data().name}</p>`;
+    childElement.addEventListener('click',() => {
+        localStorage.setItem('profile',doc.data());
+    })
+    parentElement.appendChild(childElement);
+  });
+  const childAddElement = document.createElement('div');
+  childAddElement.className = "profileinnerdetails d-flex flex-column me-3";
+  childAddElement.innerHTML = `<div class="addicon"><i class="bi bi-plus-circle-fill"></i></div><p class="iconp" style="text-align: center;">Add profile</p>`;
+  parentElement.appendChild(childAddElement);
+
+}
--- script/login.js
+++ script/login.js
@@ -1,22 +1,41 @@
-const details = [
-    {
-        mailid:"albin@gmail.com",
-        password:"albin1234"
-    },
-    {
-        mailid:"gokul@gmail.com",
-        password:"gokul1234"
-    },
-    {
-        mailid:"sarath@gmail.com",
-        password:"sarath1234"
-    },
-    {
-        mailid:"asima@gmail.com",
-        password:"asima1234"
-    },
-    {
-        mailid:"nikhil@gmail.com",
-        password:"nikhil1234"
-    }
-]
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcbi3_oCi8ZvIaVJCo_nity5rZnjpObow",
+    authDomain: "netflixclone-28d52.firebaseapp.com",
+    projectId: "netflixclone-28d52",
+    storageBucket: "netflixclone-28d52.appspot.com",
+    messagingSenderId: "478121105015",
+    appId: "1:478121105015:web:f14cdfd963421075805872"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signInButton = document.getElementById('signInButton');
+
+signInButton.addEventListener("click",() => {
+    const emailInput = document.getElementById('email-box');
+    const passwordInput = document.getElementById('password-box');
+    let email = emailInput.value;
+    console.log(email);
+    let password = passwordInput.value;
+    console.log(password);
+  
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        localStorage.setItem('userId',user.uid);
+        window.location.href = 'home.html';
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Error occurred. Try again.");
+        emailInput.setAttribute('placeholder','Invalid credentials');
+        passwordInput.setAttribute('placeholder','Invalid credentials');
+        
+      });
+  });
