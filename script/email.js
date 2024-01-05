4f37f88d46c918d451f529fa85818942b0a6f4f9
Nikhil R Bhat <nikhil.bhat@experionglobal.local>
2024-01-05 14:30:11 +0530

    fix bugs

--- script/email.js
+++ script/email.js
@@ -1,13 +1,13 @@
 import { userKEy,serviceKey,templateKey,templateKey2 } from "./config.js";
  let otp;
 export function returnOTP() {
-    // Simulate OTP generation (replace this with a secure OTP generation logic)
+   
     otp = generateOTP();
     return otp;
    
 }
 document.getElementById('getOtp').addEventListener("click",function(event){
-    console.log("reached here")
+
     event.preventDefault()
    
     let emailToSend=document.getElementById('email-signup').value;
@@ -18,33 +18,25 @@ document.getElementById('getOtp').addEventListener("click",function(event){
     document.getElementById('create-acct-btn').style.display="block";
     
     
-    console.log("Success! mail sent final");
+    
 
-    // window.alert("Success! mail sent final");
+    
    
 });
 export function sendEmailSender(userEmail,userName) {
-    console.log("Success! mail sent before final");
     
-    // window.alert("Success! mail sent before final");
-    
-    // alert("A mail is sent to your email")
-   console.log("enter");
-    // const otp=otp();
-    // Set up EmailJS parameters
     emailjs.init(userKEy);
 
-    // Set up email template parameters
     const templateParams = {
-        to_email: userEmail,  // Use the dynamically obtained user's email address
+        to_email: userEmail,  
        to_name:userName,
        otp:otp
-        // logoim:"<img src='../assets/netflix1_logo.png" + imageBase64 + "'></img>"
+       
         
         
     };
 
-    // Send email using EmailJS
+   
     if(localStorage.getItem("successKey")=="true")
     {
         emailjs.send(serviceKey, templateKey2, templateParams)
@@ -57,18 +49,18 @@ export function sendEmailSender(userEmail,userName) {
         .then(response => {
             console.log('Email sent successfully:', response);
             
-            // window.location.href = '../htmlpages/newsandpopular.html';
+           
         })
         .catch(error => {
             console.error('Error sending email:', error);
-            // Handle error, e.g., display an error message to the user
+            
             alert('Failed to send mail. Please try again.');
         });
     }
 }
 
 function generateOTP() {
-    // Simulate OTP generation (replace this with a secure OTP generation logic)
+    
     return Math.floor(100000 + Math.random() * 900000);
    
 }
--- script/signuptry.js
+++ script/signuptry.js
@@ -5,7 +5,6 @@ import { sendEmailSender } from "./email.js";
 import { firebaseConfig } from "./config.js";
 import {returnOTP} from "./email.js";
 const otp=returnOTP();
- console.log(otp)
 
 
 
@@ -28,38 +27,24 @@ buttonGet.addEventListener("click", function(event) {
 
   signupEmail = emailGet.value;
   signupPassword = passwordGet.value;
-  console.log("chekcing")
+
   userName=userNameGet.value;
-  console.log(userName)
+
   
   if(signupEmail == null || signupPassword == null) {
     window.alert("Please fill out all required fields.");
     isVerified = false;
   }
 
-  console.log(signupEmail);
-  console.log(signupPassword);
- console.log("checkotp"+checkOtp.value)
- 
- 
- console.log(otp+"new otp")
+
   if(isVerified && checkOtp.value==otp) {
       createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
           .then((userCredential) => {
             localStorage.setItem("successKey","true");
      
-              const user = userCredential.user;
-              console.log(user.uid);
-              
-              
-              console.log("trying to enter")
-             
-             
+              const user = userCredential.user;             
              sendEmailSender(signupEmail,userName);
               
-             
-
-                
             const videoElement = document.createElement("video");
             videoElement.id='videosign'
             videoElement.src = "../assets/netflixlogovideo.mp4";
@@ -67,7 +52,7 @@ buttonGet.addEventListener("click", function(event) {
             videoElement.loop = true;
             videoElement.muted = false;
             videoElement.playbackRate = 2;
-            // Set the styles for the video element
+            
             videoElement.style.width = "1150px";
             videoElement.style.height = "100vh";
             videoElement.style.position = "fixed";
@@ -76,14 +61,14 @@ buttonGet.addEventListener("click", function(event) {
             videoElement.style.zIndex = "9999";
             
 
-            // Append the video element to the body
+         
             document.body.appendChild(videoElement);
             document.body.style.overflow="hidden";
 
 
               setTimeout(function() {
                 window.location.href = '../htmlpages/login.html';
-            }, 8000); // 20000 milliseconds = 20 seconds
+            }, 8000); 
             
           })
           .catch((error) => {
